9d25a96f6136a5e9b97656ceeba3881c6c1b9919
mrizkyff <muhamadrizkyff@gmail.com>
2020-06-09 07:05:40 +0700

    update mainScreen

--- screens/mainScreen/index.js
+++ screens/mainScreen/index.js
@@ -65,41 +65,41 @@ const mainScreen = () => {
             <ScrollView style={styles.scrollviewContent}>
 
 
-            {/* awal searchbar */}
-            <SearchBar />
-            {/* akhir searchbar */}
+                {/* awal searchbar */}
+                <SearchBar />
+                {/* akhir searchbar */}
 
-            {/* awal komponen banner */}
-            <View style={styles.bannerTop}>
-                <Text style={{
-                    color: '#696969', fontSize: 17, marginLeft: 5, marginTop: 165, fontWeight
-                        : '600'
-                }}>Apa yang baru?</Text>
-                <FlatList
-                    horizontal
-                    data={banner}
-                    keyExtractor={({ id }, index) => id}
-                    renderItem={({ item }) => (
-                        <View>
-                            <Card>
-                                <CardItem>
-                                    <Body>
-                                        {/* <Text>{item.captions}</Text> */}
-                                        <Image
-                                            style={{ width: 299, height: 130, resizeMode: 'cover' }}
-                                            source={{
-                                                uri: `http://192.168.8.101/restApi-dietHouseSemarang/asset/img/banner/${item.picture}`,
-                                            }}
-                                        />
-                                        <Text style={{ color: '#696969', marginTop: 2 }}>{item.captions}</Text>
-                                    </Body>
-                                </CardItem>
-                            </Card>
-                        </View>
-                    )}
-                />
-            </View>
-            {/* akhir komponen banner */}
+                {/* awal komponen banner */}
+                <View style={styles.bannerTop}>
+                    <Text style={{
+                        color: '#696969', fontSize: 17, marginLeft: 5, marginTop: 165, fontWeight
+                            : '600'
+                    }}>Apa yang baru?</Text>
+                    <FlatList
+                        horizontal
+                        data={banner}
+                        keyExtractor={({ id }, index) => id}
+                        renderItem={({ item }) => (
+                            <View>
+                                <Card>
+                                    <CardItem>
+                                        <Body>
+                                            {/* <Text>{item.captions}</Text> */}
+                                            <Image
+                                                style={{ width: 299, height: 130, resizeMode: 'cover' }}
+                                                source={{
+                                                    uri: `http://192.168.8.101/restApi-dietHouseSemarang/asset/img/banner/${item.picture}`,
+                                                }}
+                                            />
+                                            <Text style={{ color: '#696969', marginTop: 2 }}>{item.captions}</Text>
+                                        </Body>
+                                    </CardItem>
+                                </Card>
+                            </View>
+                        )}
+                    />
+                </View>
+                {/* akhir komponen banner */}
 
 
             </ScrollView>
@@ -107,7 +107,27 @@ const mainScreen = () => {
 
 
             {/* bagian bottom navigator */}
-            <Text>Bottom Nav</Text>
+            <View style={styles.viewBottomNav}>
+                <View style={styles.viewTombolBottomNav}>
+                    <TouchableOpacity>
+                        <Icon type="MaterialIcons" name="explore" style={{ color: '#FFBF57', fontSize: 28, alignSelf: 'center' }} />
+                        <Text style={{ color: '#FFBF57', fontSize: 15, alignSelf: 'center' }}>Explore</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Icon type="MaterialIcons" name="store" style={{ color: '#BEBEBE', fontSize: 28, alignSelf: 'center' }} />
+                        <Text style={{ color: '#BEBEBE', fontSize: 15, alignSelf: 'center' }}>Belanja</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Icon type="MaterialIcons" name="shopping-basket" style={{ color: '#BEBEBE', fontSize: 28, alignSelf: 'center' }} />
+                        <Text style={{ color: '#BEBEBE', fontSize: 15, alignSelf: 'center' }}>Keranjang</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Icon type="MaterialIcons" name="account-circle" style={{ color: '#BEBEBE', fontSize: 28, alignSelf: 'center' }} />
+                        <Text style={{ color: '#BEBEBE', fontSize: 15, alignSelf: 'center' }}>Profil</Text>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
             {/* akhir bagian bottom navigator */}
 
         </View>
@@ -146,7 +166,18 @@ const styles = StyleSheet.create({
     },
     scrollviewContent: {
         width: '100%',
-        // height: '100%',
+        // h,eight: '100%',
+    },
+    viewBottomNav: {
+        width: '105%',
+        backgroundColor: 'white',
+        height: 50,
+    },
+    viewTombolBottomNav: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 3,
     }
 })
 

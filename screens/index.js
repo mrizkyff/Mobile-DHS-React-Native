4bd5a7ec5d5f300c920dddf02cc9349611b05688
mrizkyff <muhamadrizkyff@gmail.com>
2020-06-12 23:14:23 +0700

    import shoppingScreen

--- screens/index.js
+++ screens/index.js
@@ -2,5 +2,6 @@ import SplashScreen from "./splashScreen";
 import RegisterScreen from "./registerScreen";
 import LoginScreen from './loginScreen';
 import MainScreen from './mainScreen';
+import ShoppingScreen from './shoppingScreen';
 
-export {SplashScreen, RegisterScreen, LoginScreen, MainScreen};
+export {SplashScreen, RegisterScreen, LoginScreen, MainScreen, ShoppingScreen};
